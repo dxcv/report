455e6a754d976c211d783768ec1bdecb0b1126dd
CHEN KE <chenkexmrcc@qq.com>
2021-05-25 13:08:12 +0800

    new

--- report.py
+++ report.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import pymysql
+from docx import *
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 
 class Function:
@@ -71,6 +73,8 @@ class Function:
                 tmp.columns = ['业务日期', '投组单元名称', '产品分类', '名称', '市值', '到期日', '起息日']
                 self.data = self.data.append(tmp)
                 self.data = self.data.reset_index(drop=True)
+            db = pymysql.connect(host='localhost', port=3306, user='root', password='root', db='pac', charset='utf8')
+            instrument = pd.read_sql("select name from instrument_am", db)['name'].tolist()
             if os.path.exists("data/利率型.xls"):
                 tmp = pd.read_excel("data/利率型.xls")
                 tmp_index = tmp[~tmp['理财产品/内部投组名称'].isna()].index.tolist()
@@ -87,6 +91,8 @@ class Function:
                 tmp = tmp[['理财产品/内部投组名称', '投资资产明细', '资产名称', '市值', '到期日', '起息日', '买入价格/100元', '估值/100元']]
                 tmp.columns = ['投组单元名称', '产品分类', '名称', '市值', '到期日', '起息日', '成本', '估值']
                 tmp['业务日期'] = self.data.loc[0, '业务日期']
+                for x in instrument:
+                    tmp.loc[tmp['名称'] == x, '产品分类'] = '理财直接融资工具'
                 self.data = self.data.append(tmp)
                 self.data = self.data.reset_index(drop=True)
             if os.path.exists("data/净值型.xls"):
@@ -104,6 +110,8 @@ class Function:
                 tmp = tmp[['理财产品/内部投组名称', '投资资产分类', '资产名称', '市值', '到期日', '起息日', '买入价格', '百元估值']]
                 tmp.columns = ['投组单元名称', '产品分类', '名称', '市值', '到期日', '起息日', '成本', '估值']
                 tmp['业务日期'] = self.data.loc[0, '业务日期']
+                for x in instrument:
+                    tmp.loc[tmp['名称'] == x, '产品分类'] = '理财直融工具'
                 self.data = self.data.append(tmp)
                 self.data = self.data.reset_index(drop=True)
 
@@ -121,7 +129,7 @@ class Function:
             pass
 
     def __init__(self, name):
-        self.name = {"同业": self.TY(), "理财": self.LC()}.get(name)
+        self.name = {"同业业务中心": self.TY(), "理财事业部": self.LC()}.get(name)
 
     def get_asset(self):
         return self.name.asset
@@ -146,14 +154,122 @@ class Stream:
 
 class Department:
     def __init__(self, name):
+        self.name = name
         func = Function(name)
         self.bs = BalanceSheet(func.get_asset(), func.get_loan())
         self.stream = Stream(func.get_stream())
 
     def struct(self):
+        data = {}
+        tmp = self.bs.asset.groupby('产品分类', as_index=False)['市值'].sum()
+        tmp['占比'] = tmp['市值'] / tmp['市值'].sum()
+        data['资产'] = tmp
+        tmp = self.bs.loan.groupby('产品分类', as_index=False)['市值'].sum()
+        tmp['占比'] = tmp['市值'] / tmp['市值'].sum()
+        data['负债'] = tmp
+        return data
+
+    def concentration(self):
+        pass
+
+    def lever(self):
+        pass
+
+    def duration(self):
+        pass
+
+    def ratio(self):
+        pass
+
+    def fund(self):
+        pass
+
+    def etf(self):
         pass
 
 
+class Word:
+    def __init__(self):
+        self.document = Document('风险管理部金融市场风险监测报告模板.docx')
+
+    @staticmethod
+    def delete_row(table, n):
+        row = table.rows[n]
+        tbl = table._tbl
+        tr = row._tr
+        tbl.remove(tr)
+
+    def sharp_table(self, table, n):
+        total = len(table.rows)
+        if total > n:
+            for x in range(total - n):
+                self.delete_row(table, 2)
+        elif total < n:
+            raise ValueError
+
+    @staticmethod
+    def style_cell(cell, name, size, bold=False):
+        for paragraph in cell.paragraphs:
+            paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+            for run in paragraph.runs:
+                run.font.name = name
+                run.font.size = size
+                run.font.bold = bold
+
+    def go(self):
+        ty = Department("同业业务中心")
+        lc = Department("理财事业部")
+        
+        size = len(ty.struct().get('资产'))
+        if len(ty.struct().get('负债')) > size:
+            size = len(ty.struct().get('负债'))
+        self.sharp_table(self.document.tables[4], size + 4)
+        for x in range(len(ty.struct().get('资产'))):
+            self.document.tables[4].cell(2 + x, 0).text = ty.struct().get('资产').loc[x, '产品分类']
+            self.style_cell(self.document.tables[4].cell(2 + x, 0), '宋体', 177800)
+            self.document.tables[4].cell(2 + x, 1).text = str(round(ty.struct().get('资产').loc[x, '市值'], 2))
+            self.style_cell(self.document.tables[4].cell(2 + x, 1), '宋体', 177800)
+            self.document.tables[4].cell(2 + x, 2).text = str(round(ty.struct().get('资产').loc[x, '占比'] * 100, 2)) + "%"
+            self.style_cell(self.document.tables[4].cell(2 + x, 2), '宋体', 177800)
+        self.document.tables[4].cell(2 + size, 1).text = str(round(ty.struct().get('资产')['市值'].sum(), 2))
+        self.style_cell(self.document.tables[4].cell(2 + size, 1), '宋体', 177800, True)
+        for x in range(len(ty.struct().get('负债'))):
+            self.document.tables[4].cell(2 + x, 3).text = ty.struct().get('负债').loc[x, '产品分类']
+            self.style_cell(self.document.tables[4].cell(2 + x, 3), '宋体', 177800)
+            self.document.tables[4].cell(2 + x, 4).text = str(round(ty.struct().get('负债').loc[x, '市值'], 2))
+            self.style_cell(self.document.tables[4].cell(2 + x, 4), '宋体', 177800)
+            self.document.tables[4].cell(2 + x, 5).text = str(round(ty.struct().get('负债').loc[x, '占比'] * 100, 2)) + "%"
+            self.style_cell(self.document.tables[4].cell(2 + x, 5), '宋体', 177800)
+        self.document.tables[4].cell(2 + size, 4).text = str(round(ty.struct().get('负债')['市值'].sum(), 2))
+        self.style_cell(self.document.tables[4].cell(2 + size, 4), '宋体', 177800, True)
+        
+        size = len(lc.struct().get('资产'))
+        if len(lc.struct().get('负债')) > size:
+            size = len(lc.struct().get('负债'))
+        self.sharp_table(self.document.tables[5], size + 4)
+        for x in range(len(lc.struct().get('资产'))):
+            self.document.tables[5].cell(2 + x, 0).text = lc.struct().get('资产').loc[x, '产品分类']
+            self.style_cell(self.document.tables[5].cell(2 + x, 0), '宋体', 177800)
+            self.document.tables[5].cell(2 + x, 1).text = str(round(lc.struct().get('资产').loc[x, '市值'], 2))
+            self.style_cell(self.document.tables[5].cell(2 + x, 1), '宋体', 177800)
+            self.document.tables[5].cell(2 + x, 2).text = str(round(lc.struct().get('资产').loc[x, '占比'] * 100, 2)) + "%"
+            self.style_cell(self.document.tables[5].cell(2 + x, 2), '宋体', 177800)
+        self.document.tables[5].cell(2 + size, 1).text = str(round(lc.struct().get('资产')['市值'].sum(), 2))
+        self.style_cell(self.document.tables[5].cell(2 + size, 1), '宋体', 177800, True)
+        for x in range(len(lc.struct().get('负债'))):
+            self.document.tables[5].cell(2 + x, 3).text = lc.struct().get('负债').loc[x, '产品分类']
+            self.style_cell(self.document.tables[5].cell(2 + x, 3), '宋体', 177800)
+            self.document.tables[5].cell(2 + x, 4).text = str(round(lc.struct().get('负债').loc[x, '市值'], 2))
+            self.style_cell(self.document.tables[5].cell(2 + x, 4), '宋体', 177800)
+            self.document.tables[5].cell(2 + x, 5).text = str(round(lc.struct().get('负债').loc[x, '占比'] * 100, 2)) + "%"
+            self.style_cell(self.document.tables[5].cell(2 + x, 5), '宋体', 177800)
+        self.document.tables[5].cell(2 + size, 4).text = str(round(lc.struct().get('负债')['市值'].sum(), 2))
+        self.style_cell(self.document.tables[5].cell(2 + size, 4), '宋体', 177800, True)
+        
+        if os.path.exists('风险管理部金融市场风险监测报告.docx'):
+            os.remove('风险管理部金融市场风险监测报告.docx')
+        self.document.save('风险管理部金融市场风险监测报告.docx')
+
+
 if __name__ == '__main__':
-    ty = Department("同业")
-    lc = Department("理财")
+    Word().go()
